7d69883d8f5ee804d9beb878d5d9940c33b57c41
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-04 20:14:23 -0500

    got contact page working

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,6 @@ class Footer extends Component {
     render() {
         return (
             <div>
-                <h1>Foot Foot ddotooot</h1>
                 <footer class="page-footer">
                     <h6 id="footText">Made by Lauren Noeltner</h6>
                 </footer>
