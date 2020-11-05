004b1e99f1c378b277fd3a31a89272044579de59
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-04 20:06:09 -0500

    got footer working

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,6 +5,9 @@ class Footer extends Component {
         return (
             <div>
                 <h1>Foot Foot ddotooot</h1>
+                <footer class="page-footer">
+                    <h6 id="footText">Made by Lauren Noeltner</h6>
+                </footer>
             </div>
         );
     }
