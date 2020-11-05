a2edc2da6dd3af207f20bfcefcf60167b20bc8ef
Lauren Noeltner <laurennoeltner@gmail.com>
2020-11-04 20:19:37 -0500

    got About Me and Portfolio working; only have to add images to all components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,60 @@ class Home extends Component {
         return (
             <div>
                <h1>this is the Home page</h1> 
+               <main className="container">
+                <section className="row">
+                    <div className="col-xs-5 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                    <h1>About Me</h1>
+                    </div>
+                    <div className="col-xs-7 col-sm-6 col-md-8 col-lg-8 col-xl-8"></div>
+                </section>
+                <hr />
+                <section className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <img
+                        src="assets\Images\IMG_0575_83501212705938.JPG"
+                        className="mr-3"
+                        id="bioimage"
+                        alt="Lauren Noeltner Picture"
+                    />
+                    <br /><br />
+                    <p>
+                        I am a full-stack developer born and currently based in Atlanta,
+                        Georgia. I am currently attending Georgia Tech's Full-Stack Flex
+                        Boot Camp. I have always had a wide range of interests, from fine
+                        arts to the hard sciences, and I have been lucky to get a chance to
+                        explore most of them at some point in my life.
+                    </p>
+                    <p>
+                        I began at university in 2011 at the University of Georgia with a
+                        focus in Pre-Veterinary Medicine, which switched to Anthropology
+                        after a year and switched again to Psychology with a minor in
+                        Spanish and Painting after another year. I attended U.G.A. until the
+                        end of my Junior year, when I decided to leave Athens to return to
+                        Atlanta.
+                    </p>
+                    <p>
+                        Since then, I have continued to enjoy learning independently, and,
+                        in my spare time, I am almost always researching whatever fascinates
+                        me in that moment. I love learning about history, language, animals,
+                        the arts, space, computers and technology. I am currently teaching
+                        myself German after becoming mostly fluent in Spanish in college.
+                        When I am not researching interesting information, you could find me
+                        watching movies, playing games, playing with my dogs, or
+                        painting/making something.
+                    </p>
+                    <p>
+                        I am more than halfway through the Georgia Tech Coding Boot Camp,
+                        and I am greatly enjoying the opportunity to strengthen my skills as
+                        a developer. See my portfolio for some of my completed projects!
+                    </p>
+                    </div>
+                </section>
+                <section className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"></div>
+                </section>
+                <br />
+                </main>
             </div>
         );
     }
